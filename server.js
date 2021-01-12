5a435346dbdad4a7d2967f5bbf2041a4c2b22097
Lucah Endicott <lucahdavid88@gmail.com>
2021-01-12 13:43:24 -0800

    update handlebars idndex, seeds now showing

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, () =>
-  console.log(`Server listening on: http://localhost:${PORT}`)
+    console.log(`Server listening on: http://localhost:${PORT}`)
 );

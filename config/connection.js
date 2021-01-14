0bd2091a7139865ea2ec81c6ee09aa05cf97fe7c
Lucah Endicott <lucahdavid88@gmail.com>
2021-01-13 19:22:06 -0800

    update connection for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,19 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
+let connection;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'burgers_db',
-});
-
-// Make connection.
-connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-  }
-  console.log(`connected as id ${connection.threadId}`);
-});
+if (process.env.JAWSDB_URL) {
+  conenction = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db',
+  });
+}
 
 // Export connection for our ORM to use.
+connection.connect()
 module.exports = connection;

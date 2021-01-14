ac44b428a461227f8bab3a55792513eafcce8d98
Lucah Endicott <lucahdavid88@gmail.com>
2021-01-13 20:42:40 -0800

    fix connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,13 @@ const mysql = require('mysql');
 let connection;
 
 if (process.env.JAWSDB_URL) {
-  conenction = mysql.createConnection(process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   connection = mysql.createConnection({
-    host: 'lfmerukkeiac5y5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'r6spf68nc3yyqj3g',
-    password: 'xzyq4ow17pzxriww',
-    database: 'kwj19z2i5x0jqbdt',
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db',
   });
 }
 
